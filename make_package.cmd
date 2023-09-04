6260094ba88a3e29f30f126e5a3f3f5dbad357aa
Pavel <pavel.abakanov@outlook.com>
2023-09-04 20:51:55 +0200

    Changelog not copied to package

--- make_package.cmd
+++ make_package.cmd
@@ -4,9 +4,9 @@ setlocal
 set /p VERSION=<build\version.txt
 for /f "delims=" %%a in ('git log --oneline ^| find "" /v /c') do @set BUILD=%%a
 
-php -f build\update.php %VERSION% %BUILD%
-
 copy build\changelog.txt dune_plugin\ >nul
+
+php -f build\update.php %VERSION% %BUILD%
 
 pushd dune_plugin
 7z a ..\dune_plugin_proiptv.zip >nul
