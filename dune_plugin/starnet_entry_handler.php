d6f7ac3c4f49b52004b56641f3261e7ff03dc163
Pavel <pavel.abakanov@outlook.com>
2023-09-07 15:33:07 +0200

    Drop support firmware rev. < 11

--- dune_plugin/starnet_entry_handler.php
+++ dune_plugin/starnet_entry_handler.php
@@ -84,6 +84,16 @@ class Starnet_Entry_Handler implements User_Input_Handler
                 $this->plugin->epg_man->init_cache_dir();
                 switch ($user_input->action_id) {
                     case 'launch':
+                        if (!is_newer_versions()) {
+                            return  Action_Factory::show_error(true, TR::t('err_too_old_player'),
+                                array(
+                                    TR::load_string('err_too_old_player'),
+                                    "Dune Product ID: " .get_product_id(),
+                                    "Dune Firmware: " . get_raw_firmware_version(),
+                                    "Dune Serial: " . get_serial_number(),
+                                ));
+                        }
+
                         if ($this->plugin->get_playlists()->size() === 0) {
                             return User_Input_Handler_Registry::create_action($this, 'do_setup');
                         }
