9c743ee41e0184af2509d6e28e7b7afee0ea5fae
Pavel <pavel.abakanov@outlook.com>
2023-09-14 22:24:45 +0200

    Small fixes

--- dune_plugin/starnet_ext_setup_screen.php
+++ dune_plugin/starnet_ext_setup_screen.php
@@ -305,7 +305,9 @@ class Starnet_Ext_Setup_Screen extends Abstract_Controls_Screen implements User_
         flush();
 
         $this->plugin->init_plugin();
+        hd_debug_print("Reset XMLTV cache dir to default");
         $this->plugin->set_xmltv_cache_dir(null);
+        $this->plugin->epg_man->init_cache_dir();
 
         $this->plugin->tv->reload_channels($plugin_cookies);
 
