8bbe90f0c6927aa5c71d6e1544b2bacf9eb7ad80
Pavel <pavel.abakanov@outlook.com>
2023-09-19 23:42:42 +0200

    Fix script for Homatics/Boxy

--- dune_plugin/www/cgi-bin/index_epg.sh
+++ dune_plugin/www/cgi-bin/index_epg.sh
@@ -6,11 +6,15 @@ cgi_plugin_env()
   plugin_root=$(builtin cd "$thisdir/../.." && pwd)
   plugin_name=$(basename "$plugin_root")
 
+  if [ -z "$HD_HTTP_LOCAL_PORT" ]; then
+    HD_HTTP_LOCAL_PORT = "80";
+  fi
+
   export PLUGIN_NAME="$plugin_name"
   export PLUGIN_INSTALL_DIR_PATH="$plugin_root"
   export PLUGIN_TMP_DIR_PATH="$FS_PREFIX/tmp/plugins/$plugin_name"
-  export PLUGIN_WWW_URL="http://127.0.0.1/plugins/$plugin_name/"
-  export PLUGIN_CGI_URL="http://127.0.0.1/cgi-bin/plugins/$plugin_name/"
+  export PLUGIN_WWW_URL="http://127.0.0.1:$HD_HTTP_LOCAL_PORT/plugins/$plugin_name/"
+  export PLUGIN_CGI_URL="http://127.0.0.1:$HD_HTTP_LOCAL_PORT/cgi-bin/plugins/$plugin_name/"
 
   PERSISTFS_DATA_DIR_PATH="$FS_PREFIX/persistfs/plugins_data/$plugin_name"
   FLASHDATA_DATA_DIR_PATH="$FS_PREFIX/flashdata/plugins_data/$plugin_name"
