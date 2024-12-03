2d90ff10ca15356fed875a62bbb6d1919a9823b9
Pavel <pavel.abakanov@outlook.com>
2024-12-03 01:05:40 +0100

    Update scripts

--- build/update.php
+++ build/update.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/shared_scripts/crm_settings.php");
 require_once($_SERVER["DOCUMENT_ROOT"] . "/shared_scripts/MySQL.php");
 
@@ -33,19 +33,39 @@ if (!empty($firmware) && preg_match('/.+_[rb](\d{2})/', $firmware, $m)) {
 $url_params = parse_url(getenv("REQUEST_URI"));
 if (isset($url_params['query'])) {
     parse_str($url_params['query'], $params);
+    if (isset($params['dune_auth'])) {
+        $values = explode(' ', $params['dune_auth']);
+        $logbuf .= "serial     : " . $values[2] . PHP_EOL;
+        $logbuf .= "int serial : " . $values[3] . PHP_EOL;
+    }
 }
 
-if (empty($url_params['path']) || empty($revision)) {
-    header("HTTP/1.1 404 Not found");
-    echo '["error" : "This version is not supported"]';
-} else {
-    header("HTTP/1.1 200 OK");
-    $info = pathinfo($url_params['path']);
-    $new_path = ($revision < 21 ? "old/" : "current/") . $info['basename'];
-    echo file_get_contents($new_path);
+$info = pathinfo($url_params['path']);
+$ext = $info['extension'];
 
+if (empty($revision)) {
+    header("HTTP/1.1 403 Forbidden");
+    echo '["error" : "Unknown version"]';
+} else if ($ext != 'xml' && $ext != 'gz') {
+    header("HTTP/1.1 404 Not found");
+} else {
+    $new_path = ($revision < 21 ? "./old/" : "./current/") . $info['basename'];
     $logbuf .= "url path   : " . $url_params['path'] . PHP_EOL;
     $logbuf .= "new path   : " . $new_path . PHP_EOL;
+
+    header("HTTP/1.1 200 OK");
+    if ($ext == 'gz') {
+        header("Accept-Ranges: bytes");
+        header("Content-Length: " . filesize($new_path));
+        header("Content-Type: application/octet-stream");
+    } else if ($ext == 'xml') {
+        header("Content-Type: text/xml");
+    }
+
+    header("Pragma: no-cache");
+    header("Expires: -1");
+
+    readfile($new_path);
 }
 
 write_to_log($logbuf, 'update.log');
