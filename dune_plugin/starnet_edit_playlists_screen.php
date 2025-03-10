a935beec440a73523c0b37a859fa0817a960729d
Pavel <pavel.abakanov@outlook.com>
2025-03-11 00:11:03 +0100

    Enable shortcuts only for remotes with color buttons

--- dune_plugin/starnet_edit_playlists_screen.php
+++ dune_plugin/starnet_edit_playlists_screen.php
@@ -422,8 +422,10 @@ class Starnet_Edit_Playlists_Screen extends Abstract_Preloaded_Regular_Screen im
         $menu_items[] = $this->plugin->create_menu_item($this, ACTION_ITEM_DELETE, TR::t('delete'), "remove.png");
         $menu_items[] = $this->plugin->create_menu_item($this, ACTION_ITEMS_CLEAR, TR::t('clear'), "brush.png");
 
-        $menu_items[] = $this->plugin->create_menu_item($this, GuiMenuItemDef::is_separator);
-        $menu_items[] = $this->plugin->create_menu_item($this, self::ACTION_ASSIGN_SHORTCUT_POPUP, TR::t('tv_screen_assign_shortcut'));
+        if (!is_limited_apk() || dune_config_with_color_buttons()) {
+            $menu_items[] = $this->plugin->create_menu_item($this, GuiMenuItemDef::is_separator);
+            $menu_items[] = $this->plugin->create_menu_item($this, self::ACTION_ASSIGN_SHORTCUT_POPUP, TR::t('tv_screen_assign_shortcut'));
+        }
 
         return Action_Factory::show_popup_menu($menu_items);
     }
