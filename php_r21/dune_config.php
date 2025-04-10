1b45276c5a68b64ddf1153688a1c183289d3e520
Pavel <pavel.abakanov@outlook.com>
2025-04-10 19:16:40 +0200

    php converted to symlink. Added r21

--- php_r21/dune_config.php
+++ php_r21/dune_config.php
@@ -1,5 +1,4 @@
 <?php
-/** @noinspection ALL */
 ///////////////////////////////////////////////////////////////////////////
 
 const FEW_BUTTONS_REMOTE_TYPE_NONE = 0;
@@ -10,19 +9,6 @@ const FEW_BUTTONS_REMOTE_TYPE_ARROWS = 3;
 function dune_config_get_few_buttons_remote_type()
 {
     return DuneSystem::$properties['few_buttons_remote_type'];
-}
-
-function dune_config_with_color_buttons()
-{
-     $type = dune_config_get_few_buttons_remote_type();
-     switch ($type)
-     {
-         case FEW_BUTTONS_REMOTE_TYPE_SMALL:
-         case FEW_BUTTONS_REMOTE_TYPE_ARROWS:
-             return false;
-         default:
-             return true;
-     }
 }
 
 function dune_config_show_pop_up_icon_only()
